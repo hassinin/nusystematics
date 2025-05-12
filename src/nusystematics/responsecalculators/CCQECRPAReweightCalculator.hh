482619df86c516eb02d5059cc6cc3549aa784d30
Mun Jung Jung <munjung@sbndbuild03.fnal.gov>
2025-05-12 10:41:28 -0500

    include fcl pointing to the correct template file

--- src/nusystematics/responsecalculators/CCQECRPAReweightCalculator.hh
+++ src/nusystematics/responsecalculators/CCQECRPAReweightCalculator.hh
@@ -24,7 +24,7 @@ namespace nusyst {
 
     enum ENuRange {
       LowE = 0,
-      HighE = 1,
+      HighE = 1, // don't reweight for high energy
     };
 
   protected:
@@ -60,50 +60,59 @@ namespace nusyst {
 
     //printf("[CCQECRPAReweightCalculator::GetCRPAReweight] (Enu_GeV, kin_Y, kin_Z) = (%1.3f, %1.3f, %1.3f), enu_range = %d\n", Enu_GeV, bin_kin[0], kin_Z, enu_range);
 
-    static double Enu_GeV_epsil = 1E-6;
-    double Enu_GeV_ForInterp = Enu_GeV;
-    Enu_GeV_ForInterp = std::max( Enu_GeV_ForInterp, x_FirstBinCenter[enu_range] + Enu_GeV_epsil );
-    Enu_GeV_ForInterp = std::min( Enu_GeV_ForInterp, x_LastBinCenter[enu_range] - Enu_GeV_epsil );
+    if (enu_range == 0){
 
-    static double kin_Y_epsil = 1E-6;
-    double kin_Y_ForInterp = bin_kin[0];
-    kin_Y_ForInterp = std::max( kin_Y_ForInterp, y_FirstBinCenter[enu_range] + kin_Y_epsil );
-    kin_Y_ForInterp = std::min( kin_Y_ForInterp, y_LastBinCenter[enu_range] - kin_Y_epsil );
+      static double Enu_GeV_epsil = 1E-6;
+      double Enu_GeV_ForInterp = Enu_GeV;
+      Enu_GeV_ForInterp = std::max( Enu_GeV_ForInterp, x_FirstBinCenter[enu_range] + Enu_GeV_epsil );
+      Enu_GeV_ForInterp = std::min( Enu_GeV_ForInterp, x_LastBinCenter[enu_range] - Enu_GeV_epsil );
 
-    static double kin_Z_epsil = 1E-6;
-    double kin_Z_ForInterp = bin_kin[1];
-    kin_Z_ForInterp = std::max( kin_Z_ForInterp, z_FirstBinCenter[enu_range] + kin_Z_epsil );
-    kin_Z_ForInterp = std::min( kin_Z_ForInterp, z_LastBinCenter[enu_range] - kin_Z_epsil );
+      static double kin_Y_epsil = 1E-6;
+      double kin_Y_ForInterp = bin_kin[0];
+      kin_Y_ForInterp = std::max( kin_Y_ForInterp, y_FirstBinCenter[enu_range] + kin_Y_epsil );
+      kin_Y_ForInterp = std::min( kin_Y_ForInterp, y_LastBinCenter[enu_range] - kin_Y_epsil );
 
-    //printf("[CCQECRPAReweightCalculator::GetCRPAReweight] -> (Enu_GeV, kin_Y, kin_Z) = (%1.3f, %1.3f, %1.3f)\n", Enu_GeV_ForInterp, kin_Y_ForInterp, kin_Z_ForInterp);
-    double xsec_WithCRPA = map_ENuRange_to_WithCRPAXSec[enu_range]->Interpolate(Enu_GeV_ForInterp, kin_Y_ForInterp, kin_Z_ForInterp); // CV
-    double xsec_WithoutCRPA = map_ENuRange_to_WithoutCRPAXSec[enu_range]->Interpolate(Enu_GeV_ForInterp, kin_Y_ForInterp, kin_Z_ForInterp);
+      static double kin_Z_epsil = 1E-6;
+      double kin_Z_ForInterp = bin_kin[1];
+      kin_Z_ForInterp = std::max( kin_Z_ForInterp, z_FirstBinCenter[enu_range] + kin_Z_epsil );
+      kin_Z_ForInterp = std::min( kin_Z_ForInterp, z_LastBinCenter[enu_range] - kin_Z_epsil );
 
-    //printf("[CCQECRPAReweightCalculator::GetCRPAReweight] xsec (With CRPA, Without CRPA) = (%1.3f, %1.3e)\n", xsec_WithCRPA, xsec_WithoutCRPA);
+      //printf("[CCQECRPAReweightCalculator::GetCRPAReweight] -> (Enu_GeV, kin_Y, kin_Z) = (%1.3f, %1.3f, %1.3f)\n", Enu_GeV_ForInterp, kin_Y_ForInterp, kin_Z_ForInterp);
+      double xsec_WithCRPA = map_ENuRange_to_WithCRPAXSec[enu_range]->Interpolate(Enu_GeV_ForInterp, kin_Y_ForInterp, kin_Z_ForInterp); // CV
+      double xsec_WithoutCRPA = map_ENuRange_to_WithoutCRPAXSec[enu_range]->Interpolate(Enu_GeV_ForInterp, kin_Y_ForInterp, kin_Z_ForInterp);
 
-    if(xsec_WithCRPA==0.){
-/*
-      printf("[CCQECRPAReweightCalculator::GetCRPAReweight] Zero cross section for\n");
-      printf("[CCQECRPAReweightCalculator::GetCRPAReweight] (Enu_GeV, kin_Y, kin_Z) = (%1.3f, %1.3f, %1.3f), enu_range = %d\n", Enu_GeV, bin_kin[0], bin_kin[1], enu_range);
-      printf("[CCQECRPAReweightCalculator::GetCRPAReweight] -> (Enu_GeV, kin_Y, kin_Z) = (%1.3f, %1.3f, %1.3f)\n", Enu_GeV_ForInterp, kin_Y_ForInterp, kin_Z_ForInterp);
-*/
-      return 1.;
+      //printf("[CCQECRPAReweightCalculator::GetCRPAReweight] xsec (With CRPA, Without CRPA) = (%1.3f, %1.3e)\n", xsec_WithCRPA, xsec_WithoutCRPA);
+
+      if(xsec_WithCRPA==0.){
+      /*
+            printf("[CCQECRPAReweightCalculator::GetCRPAReweight] Zero cross section for\n");
+            printf("[CCQECRPAReweightCalculator::GetCRPAReweight] (Enu_GeV, kin_Y, kin_Z) = (%1.3f, %1.3f, %1.3f), enu_range = %d\n", Enu_GeV, bin_kin[0], bin_kin[1], enu_range);
+            printf("[CCQECRPAReweightCalculator::GetCRPAReweight] -> (Enu_GeV, kin_Y, kin_Z) = (%1.3f, %1.3f, %1.3f)\n", Enu_GeV_ForInterp, kin_Y_ForInterp, kin_Z_ForInterp);
+      */
+        return 1.;
+      }
+
+      double weight = ( xsec_WithCRPA * (1.-parameter_value) + xsec_WithoutCRPA * parameter_value ) / xsec_WithCRPA;
+      // std::cout << "[CCQECRPAReweightCalculator] weight = " << weight << std::endl;
+
+      // clip at 3
+      weight = std::min(weight, 3.);
+
+      if(weight!=weight){
+
+        printf("[CCQECRPAReweightCalculator::GetCRPAReweight] Nan weight for\n"); 
+        printf("[CCQECRPAReweightCalculator::GetCRPAReweight] (Enu_GeV, kin_Y, kin_Z) = (%1.3f, %1.3f, %1.3f), enu_range = %d\n", Enu_GeV, bin_kin[0], bin_kin[1], enu_range);
+        printf("[CCQECRPAReweightCalculator::GetCRPAReweight] -> (Enu_GeV, kin_Y, kin_Z) = (%1.3f, %1.3f, %1.3f)\n", Enu_GeV_ForInterp, kin_Y_ForInterp, kin_Z_ForInterp);
+        weight = 1.;
+
+      }
+
+      return weight;
     }
 
-    double weight = ( xsec_WithCRPA * (1.-parameter_value) + xsec_WithoutCRPA * parameter_value ) / xsec_WithCRPA;
-
-    //std::cout << "[CCQECRPAReweightCalculator] weight = " << weight << std::endl;
-
-    if(weight!=weight){
-
-      printf("[CCQECRPAReweightCalculator::GetCRPAReweight] Nan weight for\n"); 
-      printf("[CCQECRPAReweightCalculator::GetCRPAReweight] (Enu_GeV, kin_Y, kin_Z) = (%1.3f, %1.3f, %1.3f), enu_range = %d\n", Enu_GeV, bin_kin[0], bin_kin[1], enu_range);
-      printf("[CCQECRPAReweightCalculator::GetCRPAReweight] -> (Enu_GeV, kin_Y, kin_Z) = (%1.3f, %1.3f, %1.3f)\n", Enu_GeV_ForInterp, kin_Y_ForInterp, kin_Z_ForInterp);
-      weight = 1.;
-
+  else if (enu_range == 1){
+    return 1.;
     }
-
-    return weight;
 
   }
 
@@ -136,18 +145,9 @@ namespace nusyst {
         std::cout << "--------LOADED HISTOGRAM: " << hName << std::endl;
         map_ENuRange_to_WithoutCRPAXSec[0] = std::unique_ptr<TH3D>( GetHistogram<TH3D>(input_file, input_hist) );
       }
-      else if(hName=="HighE_WithCRPA"){
-        std::cout << "--------LOADED HISTOGRAM: " << hName << std::endl;
-        map_ENuRange_to_WithCRPAXSec[1] = std::unique_ptr<TH3D>( GetHistogram<TH3D>(input_file, input_hist) );
-      }
-      else if(hName=="HighE_WithoutCRPA"){
-        std::cout << "--------LOADED HISTOGRAM: " << hName << std::endl;
-        map_ENuRange_to_WithoutCRPAXSec[1] = std::unique_ptr<TH3D>( GetHistogram<TH3D>(input_file, input_hist) );
-      }
-
     }
 
-    for(int enu_range=0; enu_range<=1; enu_range++){
+    for(int enu_range=0; enu_range<1; enu_range++){
 
       const auto& h_WithCRPAXsec = map_ENuRange_to_WithCRPAXSec[enu_range];
 
