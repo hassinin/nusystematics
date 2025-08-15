a4e0c2aa13fb255cbc1cde7f125b65e489e1bff6
Jaesung Kim <jae.sung.kim.3426@gmail.com>
2025-08-15 00:41:22 +0000

    Revert "ValenciaMECq0q3ResponseCalc.cc modified"
    This reverts commit 64d6277ddae0a67c29b634c8566e05490c22cd24.

--- src/nusystematics/responsecalculators/ValenciaMECq0q3ResponseCalc.cc
+++ src/nusystematics/responsecalculators/ValenciaMECq0q3ResponseCalc.cc
@@ -28,18 +28,8 @@ inline double clamp(double x, double a, double b) {
 // ---------------------------------------------------------------------------
 double ValenciaMECq0q3ResponseCalc::GetCentralWeight(double q0, double q3) const
 {
-  // Histogram axis order = (x = q3, y = q0)
-  const double xMin = fHist->GetXaxis()->GetXmin();
-  const double xMax = fHist->GetXaxis()->GetXmax();
-  const double yMin = fHist->GetYaxis()->GetXmin();
-  const double yMax = fHist->GetYaxis()->GetXmax();
-
-  // If kinematics fall outside trained / provided map domain, return neutral weight
-  if (q3 < xMin || q3 > xMax || q0 < yMin || q0 > yMax) {
-    return 1.0; // do not extrapolate beyond Valencia map coverage
-  }
-
-  const double w = fHist->Interpolate(q3, q0); // bilinear inside domain
+  // TH2::Interpolate is bilinear by default.  Axis order = (x=q3,y=q0).
+  const double w = fHist->Interpolate(q3, q0);
   return clamp(w, fWmin, fWmax);
 }
 
